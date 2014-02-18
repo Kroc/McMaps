b27a3f805b67a932aa6faa0e714aa3d89ffc5c56
tazorax <tazorax@gmail.com>
2014-02-18 22:19:05 +0100

    Update colors
    Since weekly snapshot 13w42b, the fourth base color variant is multiplied by 135

--- mcmap.php
+++ mcmap.php
@@ -56,55 +56,55 @@ class McMap {
 			array ('r' => 89,	'g' => 125,	'b' => 39),	//4  Grass
 			array ('r' => 109,	'g' => 153,	'b' => 48),	//5  Grass
 			array ('r' => 127,	'g' => 178,	'b' => 56),	//6  Grass
-			array ('r' => 109,	'g' => 153,	'b' => 48),	//7  Grass
+			array ('r' => 67,	'g' => 94,	'b' => 29),	//7  Grass
 			array ('r' => 174,	'g' => 164,	'b' => 115),	//8  Sand/Gravel
 			array ('r' => 213,	'g' => 201,	'b' => 140),	//9  Sand/Gravel
 			array ('r' => 247,	'g' => 233,	'b' => 163),	//10 Sand/Gravel
-			array ('r' => 213,	'g' => 201,	'b' => 140),	//11 Sand/Gravel
+			array ('r' => 130,	'g' => 123,	'b' => 86),	//11 Sand/Gravel
 			array ('r' => 117,	'g' => 117,	'b' => 117),	//12 Other
 			array ('r' => 144,	'g' => 144,	'b' => 144),	//13 Other
 			array ('r' => 167,	'g' => 167,	'b' => 167),	//14 Other
-			array ('r' => 144,	'g' => 144,	'b' => 144),	//15 Other
-			array ('r' => 180,	'g' => 0,	'b' => 0),	//16 Lava
-			array ('r' => 220,	'g' => 0,	'b' => 0),	//17 Lava
-			array ('r' => 255,	'g' => 0,	'b' => 0),	//18 Lava
-			array ('r' => 220,	'g' => 0,	'b' => 0),	//19 Lava
-			array ('r' => 112,	'g' => 112,	'b' => 180),	//20 Ice
-			array ('r' => 138,	'g' => 138,	'b' => 220),	//21 Ice
-			array ('r' => 160,	'g' => 160,	'b' => 255),	//22 Ice
-			array ('r' => 138,	'g' => 138,	'b' => 220),	//23 Ice
-			array ('r' => 117,	'g' => 117,	'b' => 117),	//24 Other
-			array ('r' => 144,	'g' => 144,	'b' => 144),	//25 Other
-			array ('r' => 167,	'g' => 167,	'b' => 167),	//26 Other
-			array ('r' => 144,	'g' => 144,	'b' => 144),	//27 Other
-			array ('r' => 0,	'g' => 87,	'b' => 0),	//28 Leaves
-			array ('r' => 0,	'g' => 106,	'b' => 0),	//29 Leaves
-			array ('r' => 0,	'g' => 124,	'b' => 0),	//30 Leaves
-			array ('r' => 0,	'g' => 106,	'b' => 0),	//31 Leaves
+			array ('r' => 88,	'g' => 88,	'b' => 88),	//15 Other
+			array ('r' => 180,	'g' => 0,	'b' => 0),	//16 Lava/TNT
+			array ('r' => 220,	'g' => 0,	'b' => 0),	//17 Lava/TNT
+			array ('r' => 255,	'g' => 0,	'b' => 0),	//18 Lava/TNT
+			array ('r' => 135,	'g' => 0,	'b' => 0),	//19 Lava/TNT
+			array ('r' => 112,	'g' => 112,	'b' => 180),	//20 Ice/Packed Ice
+			array ('r' => 138,	'g' => 138,	'b' => 220),	//21 Ice/Packed Ice
+			array ('r' => 160,	'g' => 160,	'b' => 255),	//22 Ice/Packed Ice
+			array ('r' => 84,	'g' => 84,	'b' => 135),	//23 Ice/Packed Ice
+			array ('r' => 117,	'g' => 117,	'b' => 117),	//24 Metal
+			array ('r' => 144,	'g' => 144,	'b' => 144),	//25 Metal
+			array ('r' => 167,	'g' => 167,	'b' => 167),	//26 Metal
+			array ('r' => 88,	'g' => 88,	'b' => 88),	//27 Metal
+			array ('r' => 0,	'g' => 87,	'b' => 0),	//28 Plants
+			array ('r' => 0,	'g' => 106,	'b' => 0),	//29 Plants
+			array ('r' => 0,	'g' => 124,	'b' => 0),	//30 Plants
+			array ('r' => 0,	'g' => 65,	'b' => 0),	//31 Plants
 			array ('r' => 180,	'g' => 180,	'b' => 180),	//32 Snow
 			array ('r' => 220,	'g' => 220,	'b' => 220),	//33 Snow
 			array ('r' => 255,	'g' => 255,	'b' => 255),	//34 Snow
-			array ('r' => 220,	'g' => 220,	'b' => 220),	//35 Snow
+			array ('r' => 135,	'g' => 135,	'b' => 135),	//35 Snow
 			array ('r' => 115,	'g' => 118,	'b' => 129),	//36 Clay
 			array ('r' => 141,	'g' => 144,	'b' => 158),	//37 Clay
 			array ('r' => 164,	'g' => 168,	'b' => 184),	//38 Clay
-			array ('r' => 141,	'g' => 144,	'b' => 158),	//39 Clay
+			array ('r' => 86,	'g' => 88,	'b' => 97),	//39 Clay
 			array ('r' => 129,	'g' => 74, 	'b' => 33),	//40 Dirt
 			array ('r' => 157,	'g' => 91, 	'b' => 40),	//41 Dirt
 			array ('r' => 183,	'g' => 106,	'b' => 47),	//42 Dirt
-			array ('r' => 157,	'g' => 91, 	'b' => 40),	//43 Dirt
-			array ('r' => 79, 	'g' => 79, 	'b' => 79),	//44 Smoothstone/Cobblestone/Ore
-			array ('r' => 96, 	'g' => 96, 	'b' => 96),	//45 Smoothstone/Cobblestone/Ore
-			array ('r' => 112,	'g' => 112,	'b' => 112),	//46 Smoothstone/Cobblestone/Ore
-			array ('r' => 96, 	'g' => 96, 	'b' => 96),	//47 Smoothstone/Cobblestone/Ore
+			array ('r' => 96,	'g' => 56, 	'b' => 24),	//43 Dirt
+			array ('r' => 79, 	'g' => 79, 	'b' => 79),	//44 Stone/Cobblestone/Ore
+			array ('r' => 96, 	'g' => 96, 	'b' => 96),	//45 Stone/Cobblestone/Ore
+			array ('r' => 112,	'g' => 112,	'b' => 112),	//46 Stone/Cobblestone/Ore
+			array ('r' => 59, 	'g' => 59, 	'b' => 59),	//47 Stone/Cobblestone/Ore
 			array ('r' => 45, 	'g' => 45,	'b' => 180),	//48 Water
 			array ('r' => 55, 	'g' => 55,	'b' => 220),	//49 Water
 			array ('r' => 64, 	'g' => 64,	'b' => 255),	//50 Water
-			array ('r' => 55, 	'g' => 55,	'b' => 220),	//51 Water
+			array ('r' => 33, 	'g' => 33,	'b' => 135),	//51 Water
 			array ('r' => 73, 	'g' => 58,	'b' => 35),	//52 Log/Tree/Wood
 			array ('r' => 89, 	'g' => 71,	'b' => 43),	//53 Log/Tree/Wood
 			array ('r' => 104,	'g' => 83,	'b' => 50),	//54 Log/Tree/Wood
-			array ('r' => 89, 	'g' => 71,	'b' => 43),	//55 Log/Tree/Wood
+			array ('r' => 55, 	'g' => 43,	'b' => 26),	//55 Log/Tree/Wood
 			
 			array ('r' => 180,	'g' => 177,	'b' => 172), 	//56
 			array ('r' => 220,	'g' => 217,	'b' => 211), 	//57
